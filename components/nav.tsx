71bfa6f5b7ed5510771cda613224e798874493fa
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2023-01-30 08:32:22 +0700

    switch to tailwindcss

---
+++ components/nav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Link from 'next/link';
+
+function Nav() {
+  return (
+    <div className='w-full bg-red-200 flex p-10'>
+        Hello
+    </div>
+  )
+}
+
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Nav from '@/components/nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-     <div className='font-bold bg-red-400 w-full h-full'>hello</div>
+     <div className='font-bold  w-full h-full'>hello</div>
+     <Nav/>
     </>
   )
 }
